7d08462cfac94034b09150355569e46004aa65c5
Yogesh Hasabe <yogesh31hasabe@gmail.com>
2024-12-03 10:46:54 -0500

    Updated Train & test Files

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -28,7 +28,7 @@ def get_parser():
     parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
     parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
     parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--test_batch_size', default=100, type=int, help='Testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
@@ -52,28 +52,56 @@ def collate_fn(batch):
 
     return (torch.stack(imgs, 0), torch.from_numpy(labels), lengths)
 
+# def test():
+#     args = get_parser()
+
+#     lprnet = build_lprnet(lpr_max_len=args.lpr_max_len, phase=args.phase_train, class_num=len(CHARS), dropout_rate=args.dropout_rate)
+#     device = torch.device("cuda:0" if args.cuda else "cpu")
+#     lprnet.to(device)
+#     print("Successful to build network!")
+
+#     # load pretrained model
+#     if args.pretrained_model:
+#         lprnet.load_state_dict(torch.load(args.pretrained_model))
+#         print("load pretrained model successful!")
+#     else:
+#         print("[Error] Can't found pretrained mode, please check!")
+#         return False
+
+#     test_img_dirs = os.path.expanduser(args.test_img_dirs)
+#     test_dataset = LPRDataLoader(test_img_dirs.split(','), args.img_size, args.lpr_max_len)
+#     try:
+#         Greedy_Decode_Eval(lprnet, test_dataset, args)
+#     finally:
+#         cv2.destroyAllWindows()
+
 def test():
     args = get_parser()
+    args.test_batch_size = args.test_batch_size
 
     lprnet = build_lprnet(lpr_max_len=args.lpr_max_len, phase=args.phase_train, class_num=len(CHARS), dropout_rate=args.dropout_rate)
     device = torch.device("cuda:0" if args.cuda else "cpu")
     lprnet.to(device)
     print("Successful to build network!")
 
-    # load pretrained model
+    # Load pretrained model
     if args.pretrained_model:
-        lprnet.load_state_dict(torch.load(args.pretrained_model))
+        lprnet.load_state_dict(torch.load(args.pretrained_model, weights_only=True))
         print("load pretrained model successful!")
     else:
-        print("[Error] Can't found pretrained mode, please check!")
+        print("[Error] Can't found pretrained model, please check!")
         return False
 
     test_img_dirs = os.path.expanduser(args.test_img_dirs)
     test_dataset = LPRDataLoader(test_img_dirs.split(','), args.img_size, args.lpr_max_len)
+
     try:
         Greedy_Decode_Eval(lprnet, test_dataset, args)
     finally:
-        cv2.destroyAllWindows()
+        # Commented out because not needed in Colab
+        # cv2.destroyAllWindows()
+        pass
+
 
 def Greedy_Decode_Eval(Net, datasets, args):
     # TestNet = Net.eval()
